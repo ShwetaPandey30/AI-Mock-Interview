b813f750b3c38bc4144c6b57cc999950f0d006be
ShwetaPandey30 <pandeyshweta780@gmail.com>
2026-07-06 14:30:12 +0530

    Mongoose connectiona & UserSchema

---
+++ server/src/models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+
+const userSchema = new mongoose.Schema({
+    fullName:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password:{
+        
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    profilePicture:{
+        default:""
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+},
+{
+    timestamp:true,
+}
+);
